d499d81ef447fb34988e1b90c31db60bc85aeac8
Austin Hampton <amh107@latech.edu>
2024-07-12 08:49:25 -0400

    This code has bugs in the communicator testing, I am pushing this as the first form of the testing code

--- common/tests/unit/test_CommunicatorFactory.cpp
+++ common/tests/unit/test_CommunicatorFactory.cpp
@@ -529,55 +529,312 @@ BOOST_AUTO_TEST_CASE(testing_CommunicatorFactoryReply) {
   /************************CLIENT END****************/
 }
 
-/* COMMENTED OUT TO ENABLE PROPER TESTING HTTPCredentials
-//Starter Tests for HTTPCommunicator
-BOOST_AUTO_TEST_CASE(test_send){
-  //Create a request message
-  std::string request_message = "Hello, world!";
+BOOST_AUTO_TEST_CASE(testing_CommunicatorFactory_HTTP) {
 
-  //Send the request message
-  HTTPCommunicator communicator;
-  communicator.send(request_message);
+  std::cout << "\n*****************************" << std::endl;
+  std::cout << "Starting insecure test" << std::endl;
 
-  //Verify that the request message was sent successfully
-  BOOST_CHECK(communicator.last_sent_message().compare(request_message) ==0);
+  LogContext log_context;
+  log_context.thread_name = "test_communicator_factory";
+  CommunicatorFactory factory(log_context);
+  // Create the client communicator
+  const std::string server_id = "overlord";
+  auto server = [&]() {
+    /// Creating input parameters for constructing Communication Instance
+    SocketOptions socket_options = generateCommonOptions("test_channel");
+    socket_options.port = 1341;
+    socket_options.local_id = server_id;
+
+    CredentialFactory cred_factory;
+
+    std::unordered_map<CredentialType, std::string> cred_options;
+    cred_options[CredentialType::PUBLIC_KEY] = public_key;
+    cred_options[CredentialType::PRIVATE_KEY] = secret_key;
+    cred_options[CredentialType::SERVER_KEY] = server_key;
+
+    auto credentials = cred_factory.create(ProtocolType::HTTP, cred_options);
+
+    uint32_t timeout_on_receive = 40;
+    long timeout_on_poll = 10;
+
+    // When creating a communication channel with a client application we need
+    // to locally have a server socket. So though we have specified a server
+    // socket we will actually be communicating with a client.
+    return factory.create(socket_options, *credentials, timeout_on_receive,
+                          timeout_on_poll);
+  }();
+
+  const std::string client_id = "minion";
+  auto client = [&]() {
+    /// Creating input parameters for constructing Communication Instance
+    SocketOptions socket_options = generateCommonOptions("test_channel");
+    socket_options.class_type = SocketClassType::CLIENT;
+    socket_options.connection_life = SocketConnectionLife::INTERMITTENT;
+    socket_options.port = 1341;
+    socket_options.local_id = client_id;
+
+    CredentialFactory cred_factory;
+
+    std::unordered_map<CredentialType, std::string> cred_options;
+    cred_options[CredentialType::PUBLIC_KEY] = public_key;
+    cred_options[CredentialType::PRIVATE_KEY] = secret_key;
+    cred_options[CredentialType::SERVER_KEY] = server_key;
+
+    auto credentials = cred_factory.create(ProtocolType::HTTP, cred_options);
+
+    uint32_t timeout_on_receive = 10;
+    long timeout_on_poll = 10;
+
+    // When creating a communication channel with a server application we need
+    // to locally have a client socket. So though we have specified a client
+    // socket we will actually be communicating with the server.
+    return factory.create(socket_options, *credentials, timeout_on_receive,
+                          timeout_on_poll);
+  }();
+
+  const std::string id = "Bob";
+  const std::string key = "skeleton";
+
+  auto client_id_from_comm = client->id();
+  auto server_id_from_comm = server->id();
+  std::cout << "Client id of communicator " << client_id_from_comm << std::endl;
+  std::cout << "Server id of communicator " << server_id_from_comm << std::endl;
+  BOOST_CHECK(client_id_from_comm.compare(client_id) == 0);
+  BOOST_CHECK(server_id_from_comm.compare(server_id) == 0);
+
+  MessageFactory msg_factory;
+  const std::string token = "magic_token";
+  { // Client send
+    auto msg_from_client =
+        msg_factory.create(MessageType::STRING);
+    msg_from_client->set(MessageAttribute::ID, id);
+    msg_from_client->set(MessageAttribute::KEY, key);
+
+    auto auth_by_token_req =
+        std::make_unique<Anon::AuthenticateByTokenRequest>();
+    auth_by_token_req->set_token(token);
+
+    msg_from_client->setPayload(std::move(auth_by_token_req));
+
+    client->send(*msg_from_client);
+  }
+
+  { // Server receive
+    ICommunicator::Response response =
+        server->receive(MessageType::STRING);
+    BOOST_CHECK(response.time_out == false);
+    BOOST_CHECK(response.error == false);
+
+    std::cout << "Key is "
+              << std::get<std::string>(
+                     response.message->get(MessageAttribute::KEY))
+              << std::endl;
+    std::cout << "ID is "
+              << std::get<std::string>(
+                     response.message->get(MessageAttribute::ID))
+              << std::endl;
+    BOOST_CHECK(
+        std::get<std::string>(response.message->get(MessageAttribute::KEY))
+            .compare(key) == 0);
+    BOOST_CHECK(
+        std::get<std::string>(response.message->get(MessageAttribute::ID))
+            .compare(id) == 0);
+
+    const auto &routes = response.message->getRoutes();
+    std::cout << "Routes are " << std::endl;
+    for (const auto &route : routes) {
+      std::cout << route << std::endl;
+    }
+    BOOST_CHECK(routes.size() == 1);
+    BOOST_CHECK(routes.front().compare(client_id) == 0);
+    //FLAG HERE: May have to shift this to take into account STRINGMESSAGE payload function
+    auto google_msg_ptr =
+        std::get<::google::protobuf::Message *>(response.message->getPayload());
+    Anon::AuthenticateByTokenRequest *payload =
+        dynamic_cast<Anon::AuthenticateByTokenRequest *>(google_msg_ptr);
+
+    BOOST_CHECK(payload->token().compare(token) == 0);
+  }
 }
 
-BOOST_AUTO_TEST_CASE(test_receive) {
-    // Create a response message
-    std::string response_message = "Hello, back!";
-    
-    // Simulate receiving a response message
-    HTTPCommunicator communicator;
-    communicator.receive(response_message);
-    
-    // Verify that the received response message matches the expected message
-    BOOST_CHECK(communicator.last_received_message().compare(response_message) == 0);
-}
+ BOOST_AUTO_TEST_CASE(testing_CommunicatorFactoryReply_HTTP) {
 
-BOOST_AUTO_TEST_CASE(test_poll) {
-    // Create a response message
-    std::string response_message = "Hello, back!";
-    
-    // Simulate receiving a response message
-    HTTPCommunicator communicator;
-    communicator.poll(response_message);
-    
-    // Verify that the received response message matches the expected message
-    BOOST_CHECK(communicator.last_received_message().compare(response_message) == 0);
-}
+  LogContext log_context;
+  log_context.thread_name = "test_communicator_factory_with_reply";
+  CommunicatorFactory factory(log_context);
+  // Create the client communicator
+  const std::string server_id = "overlord";
+  auto server = [&]() {
 
-BOOST_AUTO_TEST_CASE(test_id) {
-    // Verify that the id() method returns the expected value
-    HTTPCommunicator communicator;
-    BOOST_CHECK(communicator.id().compare("hello") == 0);
-}
+    //FLAG 2: May have to check this due to socket options being different for HTTP and ZMQ
+    /// Creating input parameters for constructing Communication Instance
+    SocketOptions socket_options;
+    socket_options.scheme = URIScheme::INPROC;
+    socket_options.class_type = SocketClassType::SERVER;
+    socket_options.direction_type = SocketDirectionalityType::BIDIRECTIONAL;
+    socket_options.communication_type = SocketCommunicationType::ASYNCHRONOUS;
+    socket_options.connection_life = SocketConnectionLife::PERSISTENT;
+    socket_options.protocol_type = ProtocolType::HTTP;
+    socket_options.host = "test_channel2";
+    socket_options.port = 1343;
+    socket_options.local_id = server_id;
 
-BOOST_AUTO_TEST_CASE(test_address) {
-    // Verify that the address() method returns the expected value
-    HTTPCommunicator communicator;
-    BOOST_CHECK(communicator.address().compare("hello") == 0);
-}
-*/
+    CredentialFactory cred_factory;
 
-BOOST_AUTO_TEST_SUITE_END()
+    std::unordered_map<CredentialType, std::string> cred_options;
+    cred_options[CredentialType::PUBLIC_KEY] = public_key;
+    cred_options[CredentialType::PRIVATE_KEY] = secret_key;
+    cred_options[CredentialType::SERVER_KEY] = server_key;
+
+    auto credentials = cred_factory.create(ProtocolType::HTTP, cred_options);
+
+    uint32_t timeout_on_receive = 10;
+    long timeout_on_poll = 10;
+
+    // When creating a communication channel with a client application we need
+    // to locally have a server socket. So though we have specified a server
+    // socket we will actually be communicating with a client.
+    return factory.create(socket_options, *credentials, timeout_on_receive,
+                          timeout_on_poll);
+  }();
+
+  const std::string client_id = "minion";
+  auto client = [&]() {
+    //FLAG 3: SAME AS FLAG 2
+    /// Creating input parameters for constructing Communication Instance
+    SocketOptions socket_options;
+    socket_options.scheme = URIScheme::INPROC;
+    socket_options.class_type = SocketClassType::CLIENT;
+    socket_options.direction_type = SocketDirectionalityType::BIDIRECTIONAL;
+    socket_options.communication_type = SocketCommunicationType::ASYNCHRONOUS;
+    socket_options.connection_life = SocketConnectionLife::INTERMITTENT;
+    socket_options.protocol_type = ProtocolType::HTTP;
+    socket_options.host = "test_channel2";
+    socket_options.port = 1343;
+    socket_options.local_id = client_id;
+
+    CredentialFactory cred_factory;
+
+    std::unordered_map<CredentialType, std::string> cred_options;
+    cred_options[CredentialType::PUBLIC_KEY] = public_key;
+    cred_options[CredentialType::PRIVATE_KEY] = secret_key;
+    cred_options[CredentialType::SERVER_KEY] = server_key;
+
+    auto credentials = cred_factory.create(ProtocolType::HTTP, cred_options);
+
+    uint32_t timeout_on_receive = 10;
+    long timeout_on_poll = 10;
+
+    // When creating a communication channel with a server application we need
+    // to locally have a client socket. So though we have specified a client
+    // socket we will actually be communicating with the server.
+    return factory.create(socket_options, *credentials, timeout_on_receive,
+                          timeout_on_poll);
+  }();
+
+  const std::string id = "Bob";
+  const std::string key = "skeleton";
+
+  auto client_id_from_comm = client->id();
+  auto server_id_from_comm = server->id();
+  std::cout << "Client id of communicator " << client_id_from_comm << std::endl;
+  std::cout << "Server id of communicator " << server_id_from_comm << std::endl;
+  BOOST_CHECK(client_id_from_comm.compare(client_id) == 0);
+  BOOST_CHECK(server_id_from_comm.compare(server_id) == 0);
+
+  MessageFactory msg_factory;
+  const std::string token = "magic_token";
+
+  /************************CLIENT BEGIN****************/
+  // Client send
+  auto msg_from_client =
+      msg_factory.create(MessageType::STRING);
+  msg_from_client->set(MessageAttribute::ID, id);
+  msg_from_client->set(MessageAttribute::KEY, key);
+
+  auto auth_by_token_req = std::make_unique<Anon::AuthenticateByTokenRequest>();
+  auth_by_token_req->set_token(token);
+
+  msg_from_client->setPayload(std::move(auth_by_token_req));
+
+  client->send(*msg_from_client);
+  // Client send
+  /************************CLIENT END****************/
+
+  /************************SERVER BEGIN****************/
+  // Server receive
+  ICommunicator::Response response =
+      server->receive(MessageType::STRING);
+  BOOST_CHECK(response.time_out == false);
+  BOOST_CHECK(response.error == false);
+
+  std::cout << "Key is "
+            << std::get<std::string>(
+                   response.message->get(MessageAttribute::KEY))
+            << std::endl;
+  std::cout << "ID is "
+            << std::get<std::string>(
+                   response.message->get(MessageAttribute::ID))
+            << std::endl;
+  BOOST_CHECK(
+      std::get<std::string>(response.message->get(MessageAttribute::KEY))
+          .compare(key) == 0);
+  BOOST_CHECK(std::get<std::string>(response.message->get(MessageAttribute::ID))
+                  .compare(id) == 0);
+
+  const auto &routes = response.message->getRoutes();
+  std::cout << "Routes are " << std::endl;
+  for (const auto &route : routes) {
+    std::cout << route << std::endl;
+  }
+  BOOST_CHECK(routes.size() == 1);
+  BOOST_CHECK(routes.front().compare(client_id) == 0);
+
+  //FLAG 4: May have to change this to the STRING variant made in stringmessage
+  auto google_msg_ptr =
+      std::get<::google::protobuf::Message *>(response.message->getPayload());
+  Anon::AuthenticateByTokenRequest *payload =
+      dynamic_cast<Anon::AuthenticateByTokenRequest *>(google_msg_ptr);
+
+  BOOST_CHECK(payload->token().compare(token) == 0);
+  // Server receive
+
+  // Server send
+  auto nack_msg = msg_factory.createResponseEnvelope(*response.message);
+
+  // Create Google proto message
+  auto nack_reply = std::make_unique<Anon::NackReply>();
+  nack_reply->set_err_code(ErrorCode::ID_SERVICE_ERROR);
+  std::string error_msg = "testing_no_error";
+  nack_reply->set_err_msg(error_msg);
+
+  // Place google proto message in IMessage
+  nack_msg->setPayload(std::move(nack_reply));
+
+  server->send(*nack_msg);
+  // Server send
+  /************************SERVER END****************/
+
+  /************************CLIENT BEGIN****************/
+  // Client receive
+  ICommunicator::Response response_client =
+      client->receive(MessageType::STRING);
+  while (response_client.time_out == true) {
+    response_client = client->receive(MessageType::STRING);
+  }
+  BOOST_CHECK(response_client.error == false);
+
+  // At this point there should be no routes
+  BOOST_CHECK(response_client.message->getRoutes().size() == 0);
+  //FLAG 6: SAME AS FLAG 5
+  auto response_google_msg_ptr = std::get<::google::protobuf::Message *>(
+      response_client.message->getPayload());
+  Anon::NackReply *response_payload =
+      dynamic_cast<Anon::NackReply *>(response_google_msg_ptr);
+
+  BOOST_CHECK(response_payload->err_code() == ErrorCode::ID_SERVICE_ERROR);
+  BOOST_CHECK(response_payload->err_msg().compare(error_msg) == 0);
+
+  // Client receive
+  /************************CLIENT END****************/
+}BOOST_AUTO_TEST_SUITE_END()
